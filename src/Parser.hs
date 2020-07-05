476ca0e7a17b94c28417199e024cd81957839156
Michał Jaworski <jawor403@gmail.com>
2020-07-05 12:58:57 +0200

    Basic parser code

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,10 +2,48 @@ module Parser where
 
 import DataTypes
 
+import Control.Monad.State
+import Control.Monad.Except
+import Text.Printf
+
 parse :: String -> Either ParseError Regex
 parse = undefined
 
-data ParseError -- TODO
+data ParseError = ParseError {
+    errorPos :: Int,
+    errorDesc :: String
+}
 
 instance Show ParseError where
-    show = undefined
+    show (ParseError p d) = printf "Parse error at position %d: %s" p d
+
+curError :: String -> Cursor -> ParseError
+curError d cur = ParseError (curPos cur) d
+
+-- Basic parser type and helper functions
+type Parser = StateT Cursor (Either ParseError)
+
+pPeekChar :: Parser (Maybe Char)
+pPeekChar = gets peekChar
+
+pGetPos :: Parser Int
+pGetPos = gets curPos
+
+fromJustWithEOF :: Maybe a -> Parser a
+fromJustWithEOF m = do
+    p <- pGetPos
+    maybe (throwError $ ParseError p "Unexpected end of input") return m
+
+pGetChar :: Parser Char
+pGetChar = pPeekChar >>= fromJustWithEOF
+
+pDropChar :: Parser ()
+pDropChar = gets dropChar >>= fromJustWithEOF >>= put
+
+pPopChar :: Parser Char
+pPopChar = do
+    (c, cur') <- gets popChar >>= fromJustWithEOF
+    put cur'
+    return c
+
+
