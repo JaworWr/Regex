62e5755b822399e8cb632e917160df178e3a37b0
Michał Jaworski <jawor403@gmail.com>
2020-07-11 21:39:08 +0200

    Return repeated matchings

--- test/MatchingTest.hs
+++ test/MatchingTest.hs
@@ -90,25 +90,39 @@ tests = testGroup "Matching" [
         [
             testCase "{2, 4} Eager" $ do
                 let re = Repeat 2 (Just 4) Eager (atom 'a')
-                let res = [Matching 1 5, Matching 2 6, Matching 3 6, Matching 4 6, Matching 9 12, Matching 10 12]
+                let res = [
+                        Matching 1 5, Matching 1 4, Matching 1 3, 
+                        Matching 2 6, Matching 2 5, Matching 2 4, 
+                        Matching 3 6, Matching 3 5, 
+                        Matching 4 6, 
+                        Matching 9 12, Matching 9 11, 
+                        Matching 10 12
+                        ]
                 assertEqual findAllReError res $ findAllRe re "baaaaacadaaae"
                 assertEqual findAllReError [] $ findAllRe re ""
             ,
             testCase "{2, 4} Lazy" $ do
                 let re = Repeat 2 (Just 4) Lazy (atom 'a')
-                let res = [Matching 1 3, Matching 2 4, Matching 3 5, Matching 4 6, Matching 9 11, Matching 10 12]
+                let res = [
+                        Matching 1 3, Matching 1 4, Matching 1 5,
+                        Matching 2 4, Matching 2 5, Matching 2 6,
+                        Matching 3 5, Matching 3 6,
+                        Matching 4 6, 
+                        Matching 9 11, Matching 9 12,
+                        Matching 10 12
+                        ]
                 assertEqual findAllReError res $ findAllRe re "baaaaacadaaae"
                 assertEqual findAllReError [] $ findAllRe re ""
             ,
             testCase "{2,} Eager" $ do
                 let re = Repeat 2 Nothing Eager (atom 'a')
                 assertEqual findAllReError [] $ findAllRe re ""
-                assertEqual findAllReError [Matching 0 3, Matching 1 3] $ findAllRe re "aaa"
+                assertEqual findAllReError [Matching 0 3, Matching 0 2, Matching 1 3] $ findAllRe re "aaa"
             ,
             testCase "{2,} Lazy" $ do
                 let re = Repeat 2 Nothing Lazy (atom 'a')
                 assertEqual findAllReError [] $ findAllRe re ""
-                assertEqual findAllReError [Matching 0 2, Matching 1 3] $ findAllRe re "aaa"
+                assertEqual findAllReError [Matching 0 2, Matching 0 3, Matching 1 3] $ findAllRe re "aaa"
         ] ++
         
         map (\e -> testCase ("{1, 4}+Concat " ++ show e) $ do
